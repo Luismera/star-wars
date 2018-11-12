9e51def0fc41758f75d3e4e42dfc1e59faa8fadb
Luis Mera <luismera92@gmail.com>
2018-11-12 10:34:04 -0500

    Paginas
    Creacion de nuevas paginas
    - films
    - film
    - vehicles
    - vehicle
    - starships
    - starship

---
+++ src/pages/films/films.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-films',
+  templateUrl: 'films.html',
+})
+export class FilmsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+}
